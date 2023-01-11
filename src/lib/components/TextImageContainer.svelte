4d1ebc1bbe16cea63c4afec0c1328c99fd8ec8bc
David Bång <dadda121@gmail.com>
2023-01-11 23:00:18 +0100

    added profilepic to about and rewrote content

---
+++ src/lib/components/TextImageContainer.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let src;
+  export let alt;
+</script>
+
+<div class="flex flex-col lg:flex-row">
+  <slot />
+  <img {src} {alt} class="not-prose p-0 m-0" />
+</div>
---
+++ src/routes/pages/about/+page.svelte
@@ -0,0 +1,27 @@
+<article class="lg:mx-8">
+    <h1>About me</h1>
+    <div class="flex flex-col lg:flex-row gap-4 lg:gap-12">
+        <p class="pt-0 mt-0">
+            I have been interested in game development for a long time. My first
+            experience was with Valve's "Hammer" Editor, but I later moved to
+            GameMaker and Unity where I started to learn how to program. In
+            university, I initially studied Game Design, but it was during my
+            time there that I realized programming was what I enjoyed most about
+            developing games.
+            <br />
+            <br />
+            I've made it a personal goal to be programming language and game-engine
+            agnostic, as some are better suited for certain tasks than others, and
+            they all come with their own pros & cons. I strive to always improve
+            upon my own skills, and like to try different approaches to problems
+            I have encountered before. In a team I try to help out whenever I have
+            time, as solving problems together is both fun and an opportunity to
+            learn something new!
+        </p>
+        <img
+            src="/profilepic3.png"
+            alt="David Bång"
+            class="not-prose w-96 h-96 p-0 m-0 min-w-60 min-h-60"
+        />
+    </div>
+</article>
